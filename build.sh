c6fc9eb599daa3e21363aa8632e1be61df55ceef
shivshankarsingh-opsramp <shivshankar.singh@opsramp.com>
2020-02-24 16:07:53 +0530

    all docker compose files

---
+++ build.sh
@@ -0,0 +1,10 @@
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+for folder in elasticsearch mysql hadoop nginx postgresql zookeeper cassandra tomcat mongodb solr kafka activemq websphere ceph jboss hbase weblogic redis memcached couchbase squid postfix apache haproxy spark cockroachdb lighttpd rabbitmq
+do
+    echo "Running ${folder} ...."
+    cd ${folder}
+    sudo docker-compose up --no-start
+    echo "-------------- ${folder} Done--------------"
+    cd ..
+done
